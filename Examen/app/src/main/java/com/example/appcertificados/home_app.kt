c3d45513cd98d0c3f061849150f6f50c7b4433b9
Luis Almeida <luis.almeida01@epn.edu.ec>
2022-01-11 04:26:27 -0500

    GUI de home creada

---
+++ Examen/app/src/main/java/com/example/appcertificados/home_app.kt
@@ -0,0 +1,11 @@
+package com.example.appcertificados
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class home_app : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_app)
+    }
+}
