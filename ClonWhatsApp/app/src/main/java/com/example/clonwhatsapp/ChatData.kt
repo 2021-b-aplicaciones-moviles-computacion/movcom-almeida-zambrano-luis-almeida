ff131f5e2cf549877d541783b66fc472a390ccf1
Luis Almeida <luis.almeida01@epn.edu.ec>
2022-02-13 21:39:53 -0500

    Lista creada, interfaz de mensaje creada

---
+++ ClonWhatsApp/app/src/main/java/com/example/clonwhatsapp/ChatData.kt
@@ -0,0 +1,9 @@
+package com.example.clonwhatsapp
+
+data class ChatData(
+    val nombre_usuario:String,
+    val hora_mensaje: String,
+    val mensaje: String,
+    val cantidad_mensaje: Int,
+    val img_url:String
+    )
--- ClonWhatsApp/app/src/main/java/com/example/clonwhatsapp/MainActivity.kt
+++ ClonWhatsApp/app/src/main/java/com/example/clonwhatsapp/MainActivity.kt
@@ -4,6 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    val datos_chat = listOf(
+        ChatData(
+            "Matthew Olaoluwa",
+            "19:19",
+            "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+            0,
+            "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        ),
+        ChatData(
+            "Edwin Martins",
+            "18:47",
+            "February 9, 2015",
+            1,
+            "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        ),
+
+        ChatData(
+            "Afolabi David",
+            "18:06",
+            "La fecha del archivo es: March 23, 2013",
+            2,
+            "https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        ),
+
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
