2873c8733c20bc8cb74b36a45ed0b2d89b3cc3a0
Luis Almeida <luis.almeida01@epn.edu.ec>
2022-02-14 08:08:12 -0500

    Initial Commit

--- ClonWhatsApp/app/src/main/java/com/example/clonwhatsapp/ChatData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.clonwhatsapp
-
-data class ChatData(
-    val nombre_usuario:String,
-    val hora_mensaje: String,
-    val mensaje: String,
-    val cantidad_mensaje: Int,
-    val img_url:String
-    )
--- RvWhatsApp/app/src/main/java/com/example/rvwhatsapp/Chat.kt
+++
@@ -1,9 +0,0 @@
-package com.example.rvwhatsapp
-
-data class Chat(
-    val nombre_usuario:String,
-    val hora_mensaje: String,
-    val mensaje: String,
-    val cantidad_mensaje: Int,
-    val img_url:String
-)
