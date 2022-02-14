893449836e16ededf76ecc1e63a874262b1e54e6
Luis Almeida <luis.almeida01@epn.edu.ec>
2022-02-14 08:32:44 -0500

    Vista creada para el Item del RecyclerView

---
+++ RecyclerWhatsApp/app/src/main/java/com/example/recyclerwhatsapp/SuperHeroProvider.kt
@@ -0,0 +1,104 @@
+package com.example.recyclerwhatsapp
+
+class SuperHeroProvider {
+    companion object{
+        val superheroList = listOf(
+            SuperHero(
+                "Batman",
+                "DC",
+                "Bruce Wayne",
+                "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+            ),
+            SuperHero(
+                "Luis Almeida",
+                "Marvel",
+                "Luis Almeida",
+                "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+            ),
+            SuperHero(
+                "Luis Almeida",
+                "Marvel",
+                "Luis Almeida",
+                "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+            ),
+            SuperHero(
+                "Luis Almeida",
+                "Marvel",
+                "Luis Almeida",
+                "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+            ),
+            SuperHero(
+                "Luis Almeida",
+                "Marvel",
+                "Luis Almeida",
+                "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+            ),
+            SuperHero(
+                "Luis Almeida",
+                "Marvel",
+                "Luis Almeida",
+                "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+            ),
+            SuperHero(
+                "Luis Almeida",
+                "Marvel",
+                "Luis Almeida",
+                "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+            ),
+            SuperHero(
+                "Luis Almeida",
+                "Marvel",
+                "Luis Almeida",
+                "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+            ),
+            SuperHero(
+                "Luis Almeida",
+                "Marvel",
+                "Luis Almeida",
+                "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+            ),
+            SuperHero(
+                "Luis Almeida",
+                "Marvel",
+                "Luis Almeida",
+                "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+            ),
+            SuperHero(
+                "Luis Almeida",
+                "Marvel",
+                "Luis Almeida",
+                "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+            ),
+            SuperHero(
+                "Luis Almeida",
+                "Marvel",
+                "Luis Almeida",
+                "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+            ),
+            SuperHero(
+                "Luis Almeida",
+                "Marvel",
+                "Luis Almeida",
+                "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+            ),
+            SuperHero(
+                "Luis Almeida",
+                "Marvel",
+                "Luis Almeida",
+                "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+            ),
+            SuperHero(
+                "Luis Almeida",
+                "Marvel",
+                "Luis Almeida",
+                "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+            ),
+            SuperHero(
+                "Luis Almeida",
+                "Marvel",
+                "Luis Almeida",
+                "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+            )
+        )
+    }
+}
