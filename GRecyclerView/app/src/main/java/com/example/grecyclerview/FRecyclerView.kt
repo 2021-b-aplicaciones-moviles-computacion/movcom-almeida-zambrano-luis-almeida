1fd2d3dcd9cc9154fce115a2d16805de1f8b26be
Luis Almeida <luis.almeida01@epn.edu.ec>
2022-03-08 04:12:26 -0500

    initial commit

--- GRecyclerView/app/src/main/java/com/example/grecyclerview/FRecyclerView.kt
+++
@@ -1,9 +0,0 @@
-package com.example.grecyclerview
-
-import androidx.recyclerview.widget.RecyclerView
-
-class FRecyclerView (
-    private val contexto: GrecyclerVieW,
-    private val listaEntrenador: List <BEntrenador>,
-    private val recyclerView: RecyclerView
-)
